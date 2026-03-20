c6fe60e422bea6aa24f88aaa8913bdfedc3f9453
Ruslan Rasulov <r.rasulov@dropp.market>
2026-03-20 15:13:07 +0300

    настроили все для деплоя

---
+++ ecosystem.config.js
@@ -0,0 +1,5 @@
+module.exports = [{
+  script: 'dist/main.js',
+  name: 'kupipodariday',
+  exec_mode: 'fork',
+}];
--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,8 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: true,
+    credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
 }
